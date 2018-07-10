becf948de77cd45e6f35583e9da37019252e1d68
Mario HD <mariohdpz@gmail.com>
2018-07-09 19:19:44 -0500

    Fix pyenv dependencies

--- installPyenv.sh
+++ installPyenv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev
+
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
