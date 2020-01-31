7c856bcf0d53568e1b5d715c7fe9bb7479827106
Mario <mariohdpz@gmail.com>
2020-01-30 22:46:04 -0600

    Docker Compose & Google drive file download

---
+++ GoogleDrive/download-file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+fileid="<file-id>"
+filename="<file-name>"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
