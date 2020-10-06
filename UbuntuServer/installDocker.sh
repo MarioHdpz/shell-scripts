d0428e523f0d5d502cdc1ee20dfa0d6e40a3ca0c
Mario <mariohdpz@gmail.com>
2020-10-06 16:19:20 -0500

    Docker install for Ubuntu Server 20.04

---
+++ UbuntuServer/installDocker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Install Docker in Ubuntu 20.04
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
