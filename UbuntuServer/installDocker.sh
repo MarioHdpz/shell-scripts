386ee1a00a3c0d51e03277f71107267341362ef9
Mario <mariohdpz@gmail.com>
2021-08-26 13:18:55 -0500

    Update docker compose to v2

--- UbuntuServer/installDocker.sh
+++ UbuntuServer/installDocker.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-# Install Docker in Ubuntu 20.04
+# Installs Docker in Ubuntu 20.04 - amd64
+# Installs docker compose v2
+
+DOCKER_COMPOSE_VERSION="v2.0.0-rc.1"
 
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+
+mkdir -p ${HOME}/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose-cli/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-linux-amd64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ${HOME}/.docker/cli-plugins/docker-compose
